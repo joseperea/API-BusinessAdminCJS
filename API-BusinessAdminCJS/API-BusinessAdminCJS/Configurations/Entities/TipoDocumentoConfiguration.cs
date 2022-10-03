0f9edb73ccfcc408574c067981fadd82f0354813
joseperea <31392012+joseperea@users.noreply.github.com>
2022-10-02 21:49:08 -0500

    Configurations

---
+++ API-BusinessAdminCJS/API-BusinessAdminCJS/Configurations/Entities/TipoDocumentoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using API_BusinessAdminCJS.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API_BusinessAdminCJS.Configurations.Entities
+{
+    public class TipoDocumentoConfiguration : IEntityTypeConfiguration<TipoDocumento>
+    {
+        public void Configure(EntityTypeBuilder<TipoDocumento> builder)
+        {
+            builder.HasData(
+                new TipoDocumento { Id = 1, Nombre = "C.C", Descripcion = "Cédula de ciudadanía", Estado = true }
+                                                       , new TipoDocumento { Id = 2, Nombre = "T.I", Descripcion = "Tarjeta de identidad", Estado = true }
+                                                       , new TipoDocumento { Id = 3, Nombre = "C.E", Descripcion = "Cédula de extranjería", Estado = true }
+                                                       , new TipoDocumento { Id = 4, Nombre = "T.E", Descripcion = "Tarjeta de extranjería", Estado = true }
+                );
+        }
+    }
+}
---
+++ API-BusinessAdminCJS/API-BusinessAdminCJS/Configurations/MapperInitializer.cs
@@ -0,0 +1,17 @@
+﻿using API_BusinessAdminCJS.Data.Entities;
+using API_BusinessAdminCJS.ModelsView;
+using AutoMapper;
+
+namespace API_BusinessAdminCJS.Configurations
+{
+    public class MapperInitializer : Profile
+    {
+        public MapperInitializer()
+        {
+            CreateMap<TipoDocumento, TipoDocumentoDTO>().ReverseMap();
+            CreateMap<TipoDocumento, CreateTipoDocumentoDTO>().ReverseMap();
+            CreateMap<Usuario, UsuarioDTO>().ReverseMap();
+            CreateMap<Usuario, LoginUserDTO>().ReverseMap();
+        }
+    }
+}
