5be84350978315a98b1efe233f8ca7fe34b4aec4
joseperea <31392012+joseperea@users.noreply.github.com>
2022-11-07 07:38:20 -0500

    Avances

--- API-BusinessAdminCJS/API-BusinessAdminCJS/Program.cs
+++ API-BusinessAdminCJS/API-BusinessAdminCJS/Program.cs
@@ -1,14 +1,19 @@
+using API_BusinessAdminCJS;
 using API_BusinessAdminCJS.Configurations;
 using API_BusinessAdminCJS.Data;
 using API_BusinessAdminCJS.Data.Entities;
 using API_BusinessAdminCJS.Helpers;
 using API_BusinessAdminCJS.IRepository;
 using API_BusinessAdminCJS.Repository;
+using API_BusinessAdminCJS.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using NuGet.Common;
 using Serilog;
 using Serilog.Events;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,11 +29,38 @@ Log.Information("Inicio de Aplicacíon");
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddCors(o => { o.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());});
+builder.Services.AddCors(o => { o.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => 
+builder.Services.AddSwaggerGen(c =>
 {
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = @"JWT Encabezado de autorización usando el esquema Bearer. 
+                        Ingrese 'Bearer'[espacio] y luego su token en la entrada de texto a continuación.
+                        Ejemplo: 'Bearer 12345abcdef'",
+        Name = "Autorización",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "0auth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API BusinessAdmin CJS", Version = "v1" });
 });
 
@@ -39,28 +71,31 @@ builder.Services.AddDbContext<DataContext>(o =>
 
 
 builder.Services.AddAuthentication();
+builder.Services.ConfigureIdentity();
+builder.Services.ConfigureJwt(builder.Configuration);
 //Extensions
-builder.Services.AddIdentity<Usuario, IdentityRole>(cfg =>
-{
-    cfg.Tokens.AuthenticatorTokenProvider = TokenOptions.DefaultAuthenticatorProvider;
-    cfg.SignIn.RequireConfirmedEmail = true;
-    cfg.User.RequireUniqueEmail = true;
-    cfg.Password.RequireDigit = true;
-    cfg.Password.RequiredUniqueChars = 0;
-    cfg.Password.RequireLowercase = true;
-    cfg.Password.RequireNonAlphanumeric = true;
-    cfg.Password.RequireUppercase = true;
-    cfg.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    cfg.Lockout.MaxFailedAccessAttempts = 3;
-    cfg.Lockout.AllowedForNewUsers = true;
-})
-    .AddDefaultTokenProviders()
-    .AddEntityFrameworkStores<DataContext>();
+//builder.Services.AddIdentity<Usuario, IdentityRole>(cfg =>
+//{
+//    //cfg.Tokens.AuthenticatorTokenProvider = TokenOptions.DefaultAuthenticatorProvider;
+//    //cfg.SignIn.RequireConfirmedEmail = true;
+//    //cfg.User.RequireUniqueEmail = true;
+//    ////cfg.Password.RequireDigit = true;
+//    ////cfg.Password.RequiredUniqueChars = 0;
+//    ////cfg.Password.RequireLowercase = true;
+//    ////cfg.Password.RequireNonAlphanumeric = true;
+//    ////cfg.Password.RequireUppercase = true;
+//    //cfg.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+//    //cfg.Lockout.MaxFailedAccessAttempts = 3;
+//    //cfg.Lockout.AllowedForNewUsers = true;
+//})
+//    .AddDefaultTokenProviders()
+//    .AddEntityFrameworkStores<DataContext>();
 
 builder.Services.AddScoped<IApiService, ApiService>();
+builder.Services.AddScoped<IAuthManager, AuthManager>();
 builder.Services.AddAutoMapper(typeof(MapperInitializer));
 builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
-builder.Services.AddControllers().AddNewtonsoftJson(op => op.SerializerSettings.ReferenceLoopHandling = 
+builder.Services.AddControllers().AddNewtonsoftJson(op => op.SerializerSettings.ReferenceLoopHandling =
                                                                 Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
 var app = builder.Build();
@@ -74,6 +109,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
+app.UseAuthentication();
 app.UseAuthorization();
 
 //app.MapControllerRoute(name:"default",pattern:"{controller=Home}/{action=Index}/{id?}");
--- API-BusinessAdminCJS/API-BusinessAdminCJS/ServiceExtensions.cs
+++ API-BusinessAdminCJS/API-BusinessAdminCJS/ServiceExtensions.cs
@@ -1,7 +1,44 @@
-﻿namespace API_BusinessAdminCJS
+﻿using API_BusinessAdminCJS.Data;
+using API_BusinessAdminCJS.Data.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API_BusinessAdminCJS
 {
     public static class ServiceExtensions
     {
+        public static void ConfigureIdentity(this IServiceCollection services) 
+        {
+            var builder = services.AddIdentityCore<Usuario>(q => { q.User.RequireUniqueEmail = true; });
 
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
+
+            builder.AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
+
+        }
+
+        public static void ConfigureJwt(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var jwtsettings = configuration.GetSection("Jwt");
+            var key = Environment.GetEnvironmentVariable("KEY");
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o => {
+                o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtsettings.GetSection("Issuer").Value,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+                };            
+            });
+        }
     }
 }
